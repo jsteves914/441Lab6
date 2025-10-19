3d1478fbf49c127f450248014314c93d303cf2a3
jsteves914 <jsteve10@terpmail.umd.edu>
2025-10-19 18:44:51 -0400

    Implement Bug class with threading and shifting

---
+++ Part5.py
@@ -0,0 +1,52 @@
+# Part5.py
+import time, random, threading
+from Part3 import Shifter
+
+class Bug:
+    def __init__(self, timestep=0.1, x=3, isWrapOn=False):
+        self.timestep = float(timestep)
+        self.x = int(x)
+        self.isWrapOn = bool(isWrapOn)
+
+        serial = int(input("Enter the serial/data pin (BCM): "))
+        clock = int(input("Enter the clock pin (BCM): "))
+        latch = int(input("Enter the latch pin (BCM): "))
+
+        self.__shifter = Shifter(serial, clock, latch)
+        self.__running = False
+        self.__thread = None
+
+    def __step_once(self):
+        step = random.choice([-1, 1])
+        if self.isWrapOn:
+            self.x = (self.x + step) % 8
+        else:
+            if self.x == 0 and step == -1:
+                step = 1
+            elif self.x == 7 and step == 1:
+                step = -1
+            self.x += step
+
+    def start(self):
+        if self.__running:
+            return
+        self.__running = True
+
+        def loop():
+            try:
+                while self.__running:
+                    self.__shifter.shiftByte(1 << self.x)
+                    time.sleep(self.timestep)
+                    self.__step_once()
+            finally:
+                self.__shifter.shiftByte(0)
+
+        self.__thread = threading.Thread(target=loop, daemon=True)
+        self.__thread.start()
+
+    def stop(self):
+        self.__running = False
+        if self.__thread:
+            self.__thread.join()
+        self.__shifter.shiftByte(0)
+        self.__shifter.cleanup()
